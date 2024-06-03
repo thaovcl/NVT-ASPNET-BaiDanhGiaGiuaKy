c870fdc865e2de4e423d7f21379c95a570beb432
thaovcl <nguyenvanthao170524@gmail.com>
2024-06-03 11:09:22 +0700

    add file

---
+++ NVTBaiDanhGiaGiuaKy/Controllers/NVTProductController.cs
@@ -0,0 +1,95 @@
+﻿using NVTBaiDanhGiaGiuaKy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NVTBaiDanhGiaGiuaKy.Controllers
+{
+    public class NVTProductController : Controller
+    {
+        private static List<NvtProduct> nvtProducts = new List<NvtProduct>()
+        {
+            new NvtProduct(){NvtId=221090012,NvtFullName="Nguyễn Văn Thạo",NvtImage="https://example.com/laptop.jpg",NvtQuantity=10,NvtPrice=1500,NvtisActive=true},
+            new NvtProduct(){NvtId=221090011,NvtFullName="Nguyễn Thạo Văn",NvtImage="https://example.com/laptop.jpg",NvtQuantity=10,NvtPrice=1500,NvtisActive=true},
+        };
+        // GET: NVT
+        public ActionResult NvtIndex()
+        {
+            return View(nvtProducts);
+        }
+
+        public ActionResult NvtCreate()
+        {
+            var nvtModel = new NvtProduct();
+            return View(nvtModel);
+        }
+
+        [HttpPost]
+         public ActionResult NvtCreate(NvtProduct newNvtProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(newNvtProduct);
+            }
+
+            // Tạo ID mới cho sản phẩm
+            if (nvtProducts.Any())
+            {
+                newNvtProduct.NvtId = nvtProducts.Max(p => p.NvtId) + 1;
+            }
+            else
+            {
+                newNvtProduct.NvtId = 1; // Nếu danh sách rỗng, bắt đầu với ID = 1
+            }
+
+            nvtProducts.Add(newNvtProduct);
+            return RedirectToAction("NvtIndex");
+        }
+        public ActionResult NvtEdit(int id)
+        {
+            var product = nvtProducts.Find(p => p.NvtId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+
+        }
+        public ActionResult NvtDetails(int id)
+        {
+            var product = nvtProducts.Find(p => p.LvhId == id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LvhEdit(NvtProduct lvhProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(lvhProduct);
+            }
+
+            var product = NvtProducts.Find(p => p.LvhId == lvhProduct.LvhId);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            // Cập nhật các giá trị
+            product.LvhFullName = lvhProduct.NvtFullName;
+            product.LvhImage = lvhProduct.NvtImage;
+            product.LvhQuantity = lvhProduct.NvtQuantity;
+            product.LvhPrice = lvhProduct.NvtPrice;
+            product.LvhIsActive = lvhProduct.NvtisActive;
+
+            return RedirectToAction("LvhIndex");
+        }
+    }
+}
---
+++ NVTBaiDanhGiaGiuaKy/Models/NvtProduct.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NVTBaiDanhGiaGiuaKy.Models
+{
+    public class NvtProduct
+    {
+        public int NvtId { get; set; }
+
+        [DisplayName("NVT: Họ và tên")]
+        [Required(ErrorMessage = "NVT: chưa nhập dữ liệu")]
+        [StringLength(100, MinimumLength = 5, ErrorMessage = "NVT: Họ tên chứa tối thiểu 5 ký tự và tối đa 100")]
+        [RegularExpression(@"[A-Za-z]+", ErrorMessage = "NVT: Họ tên chỉ chứa các kí tự hoặc khoảng trắng")]
+        public string NvtFullName { get; set; }
+
+        [DisplayName("NVT: Ảnh")]
+        [Required(ErrorMessage = "NVT: chưa nhập dữ liệu")]
+        public string NvtImage { get; set; }
+
+        [DisplayName("NVT: Số Lượng")]
+        [Required(ErrorMessage = "NVT: chưa nhập dữ liệu")]
+        [Range(1, 100, ErrorMessage = "NVT: Số lượng từ 1 đén 100")]
+        public int NvtQuantity { get; set; }
+
+        [DisplayName("NVT: Giá")]
+        [Required(ErrorMessage = "NVT: chưa nhập dữ liệu")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "NVT:Giá phải lớn hơn 0")]
+        public decimal NvtPrice { get; set; }
+
+
+        [Required(ErrorMessage = "NVT: chưa nhập dữ liệu")]
+        public bool NvtisActive { get; set; }
+
+    }
+}
